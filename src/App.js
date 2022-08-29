d3ea68aab925beb413c6dbad925c71bf2f70aa31
greensadfrog <deanwenchester13@gmail.com>
2022-08-29 22:42:24 +0300

    changed file tree

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
   return (
     <Router>
         <Routes>
-          <Route path="/" element={<QuizSelect/>} exact />
           <Route path="/:topic/random" element={<RandomQuestionsQuiz/>} exact />
         </Routes>
     </Router>
